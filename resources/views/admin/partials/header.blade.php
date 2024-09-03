1bc230d93a47f2a428636da5cad35df0e191d82d
bayurespati <respatibayu48@gmail.com>
2024-09-03 11:23:19 +0700

    change name admin

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -55,7 +55,7 @@ border-top: 3px solid #64B6AC;
             <a href="#" data-bs-toggle="dropdown" class="d-flex align-items-center">
                 <div class="profile-info d-flex align-items-center" data-lock-name="John Doe" data-lock-email="johndoe@okler.com">
                     <span style="color: white;">Selamat Datang,&nbsp;&nbsp;</span>
-                    <span class="name" style="font-weight: bold; color:white;">Donny Jaya Kusuma</span>
+                    <span class="name" style="font-weight: bold; color:white;">{{Auth::user()->nama}}</span>
                 </div>
                 <figure class="profile-picture">
                     <img src="/admin/img/!logged-user.jpg" alt="Joseph Doe" class="rounded-circle" data-lock-picture="img/!logged-user.jpg" />
